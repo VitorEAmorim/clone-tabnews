f8e40af11b66516fb13cde4ea743c12afcfaab0e
Vitor E. Amorim <103964391+VitorEAmorim@users.noreply.github.com>
2025-12-17 02:33:32 +0000

    Mudança de frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Não tenha medo do caminho, tenha medo de não caminhar</h1>
+    return <h1>Não tenha medo do caminho, tenha medo de não caminhar!</h1>
 }
 
 export default Home;

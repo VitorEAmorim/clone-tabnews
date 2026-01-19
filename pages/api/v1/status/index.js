5b5a68ecac6e060833573983441d5400d93c6bfa
Vitor E. Amorim <103964391+VitorEAmorim@users.noreply.github.com>
2026-01-19 23:19:00 +0000

    Retornando status de serviço do banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js";
+import { Query } from "pg";
 
 async function status(request, response){
-  const result = await database.query("SELECT 1 + 1;")
-  console.log(result)
-  response.status(200).json({"chave":"São acima da média"})
+  const updateAt = new Date();
+
+  const versionPostgres = await database.query("SHOW server_version;");
+  const versionPostgresValue = versionPostgres.rows[0].server_version
+
+  const maxQuerys = await database.query('SHOW max_connections;');
+  const maxQuerysValue = await parseInt(maxQuerys.rows[0].max_connections);
+
+  const dataBaseConnectionsResolve = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';")
+  const dataBaseOpenedConnectionsValue = await dataBaseConnectionsResolve.rows[0].count
+
+
+  response.status(200).json({
+    update_at: updateAt,
+    version : versionPostgresValue,
+    max_querys : maxQuerysValue,
+    opened_connections : dataBaseOpenedConnectionsValue
+  });
+
+
 }
 
 export default status
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,4 +2,13 @@
 test ("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http:localhost:3000/api/v1/status")
   expect(response.status).toBe(200)
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  expect(responseBody.version).toBe('16.11');
+
+  expect(responseBody.max_querys).toEqual(100);
+
+  expect(responseBody.opened_connections).toEqual(1);
 })
